3283699ded2d3dbfc5a8fb01e2440e8f7b94cdbb
Viktor Petruk <damnnn261@gmail.com>
2024-11-16 19:30:38 +0200

    create components => layout => menu.data.ts

--- components/layout/menu.data.ts
+++ components/layout/menu.data.ts
@@ -1,13 +1,48 @@
 export interface MenuData {
 	name:string,
 	icon:string,
-	path:string,
+	url:string,
 }
 
 export const MENU_DATA:MenuData[] = [
 	{
 		name:'Home',
-		icon:,
-		path:'/',
+		icon:'radix-icons:dashboard',
+		url:'/',
+	},
+	{
+		name:'Customers',
+		icon:'fluent:receipt-28-regular',
+		url:'/customers',
+	},
+	{
+		name:'Payments',
+		icon:'ph:contactless-payment',
+		url:'/payments',
+	},
+	{
+		name:'Products',
+		icon:'ep:goods',
+		url:'/products',
+	},
+	{
+		icon: 'mingcute:group-line',
+		name:'Orders',
+		url:'/orders'
+	},
+	{
+		icon: 'fluent:person-feedback-48-regular',
+		name:'Feedback',
+		url:'/feedback'
+	},
+	{
+		icon: 'radix-icons:gear',
+		name:'Settings',
+		url:'/settings'
+	},
+	{
+		icon: 'radix-icons:question-mark',
+		name:'Help center',
+		url:'/help'
 	}
 ]
